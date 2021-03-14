f4c0f87233027b5e07766b1779d573584747ec2e
Ralf Lopez <ralflopez3@gmail.com>
2021-03-14 23:14:18 +0800

    for heroku

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const app = express();
 const server = require('http').createServer(app);
 const socket = require('./utils/sockets');
 const connectdb = require('./utils/mongodb-config');
+const cors = require('cors');
+
+connectdb();
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+;}
 
 //server
 socket.addSocket(server);
